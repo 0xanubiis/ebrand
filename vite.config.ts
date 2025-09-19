27bed748b91df5d50811055287ddabe1fb395288
0xanubiis <amoriamori3016@gmail.com>
2025-09-19 11:05:14 +0300

    fix errors build

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,24 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/", // ensures correct asset resolution on Vercel
   server: {
     host: "::",
     port: 8080,
   },
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+        },
+      },
+    },
+  },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
@@ -20,3 +30,4 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
+
